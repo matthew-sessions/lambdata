942b13e0d433c00a7cf4531547428d0495aa6c9c
Matthew Sessions <53715422+matthew-sessions@users.noreply.github.com>
2019-10-01 10:48:13 -0700

    Update __init__.py

--- lambdata_matthew/__init__.py
+++ lambdata_matthew/__init__.py
@@ -3,7 +3,10 @@ import numpy as np
 
 ONES = pd.DataFrame(np.ones(30))
 
+
 def add_col(df, li,name):
+  #add new column to data frame. 
+  #Insert a dataframe, list and name of the new column  
   df = df
   df[name] = li
   return(df)
